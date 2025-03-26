f4aaa7f8ba7548ff869ae12b70ab9682ff9a8316
Payal Solanki <payalsolanki@Payals-MacBook-Air.local>
2025-03-26 18:35:35 -0400

    file updated

--- model.py
+++ model.py
@@ -1,19 +1,12 @@
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision.models as models
 
 class MyCIFARModel(nn.Module):
     def __init__(self):
         super(MyCIFARModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 8 * 8, 512)
-        self.fc2 = nn.Linear(512, 10)
+        self.model = models.resnet18(pretrained=True)
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, 10)  # For 10 CIFAR-10 classes
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        return self.model(x)
