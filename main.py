d3ff37e150ca9828b71a22d048128b5783b13f2f
Payal Solanki <payalsolanki@Payals-MacBook-Air.local>
2025-03-26 22:00:04 -0400

    file updated

--- main.py
+++ main.py
@@ -1,35 +1,3 @@
-import torch.nn as nn
-import torchvision.models as models
+model = torch.load("cifar10_model_full.pth", map_location=torch.device("cpu"))
+model.eval()
 
-class MyCIFARModel(nn.Module):
-    def __init__(self):
-        super(MyCIFARModel, self).__init__()
-        model = models.resnet18(pretrained=True)
-        num_ftrs = model.fc.in_features
-        model.fc = nn.Linear(num_ftrs, 10)
-
-        # Flatten out to current class
-        self.conv1 = model.conv1
-        self.bn1 = model.bn1
-        self.relu = model.relu
-        self.maxpool = model.maxpool
-        self.layer1 = model.layer1
-        self.layer2 = model.layer2
-        self.layer3 = model.layer3
-        self.layer4 = model.layer4
-        self.avgpool = model.avgpool
-        self.fc = model.fc
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        return x
